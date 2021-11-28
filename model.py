589449df37b99858acecaedd073829f81a01080e
jagdishj <jagdish.junjur@gmail.com>
2021-11-29 01:04:55 +0530

    Added 3 new model code

--- model.py
+++ model.py
@@ -1,1 +1,12 @@
 print("adding all ML related code here")
+
+
+def model1():
+    print("Adding code for ML Model 1")
+
+
+def model2():
+    print("Adding code for ML Model 2")
+
+def model3():
+    print("Adding code for ML Model 3")
